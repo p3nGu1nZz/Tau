acfdd1201799d1dbd8bea0c7c6b72d8789b66ec2
p3n <rawsonkara@gmail.com>
2024-09-06 17:41:19 -0400

    improve logging

--- MLAgentsProject/Scripts/ophrase_util.py
+++ MLAgentsProject/Scripts/ophrase_util.py
@@ -1,8 +1,9 @@
-from loguru import logger as log
 from typing import List, Dict, Any
+from ophrase_log import Log
 
 def post_process(text: str, results: List[Dict[str, Any]], prompts: List[str], include_prompts: bool) -> Dict[str, Any]:
     if isinstance(results, dict) and 'error' in results:
+        Log.error("Error in results")
         return results
     combined_responses = []
     for result in results:
